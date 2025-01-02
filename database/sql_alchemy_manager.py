716e9f011ee0f8763c1eda03c2874182fadb3efe
mwelch21 <welchie1321@gmail.com>
2025-01-02 13:16:27 -0500

    Read and upload

--- database/sql_alchemy_manager.py
+++ database/sql_alchemy_manager.py
@@ -6,11 +6,11 @@ class BaseEngine:
 
     def __init__(
             self,
-            user: str = 'root',
-            password: str = 'pwd',
+            user: str = 'postgres',
+            password: str = '1321',
             host: str = '127.0.0.1',
-            database: str = 'workouts',
-            port: int = 5532
+            database: str = 'workout-dev',
+            port: int = 5432
     ):
         self.user = user
         self.password = password
@@ -18,18 +18,21 @@ class BaseEngine:
         self.database = database
         self.port = port
 
-        # sql alch
+        # sql alchemy __init__(s)
         self.engine = None
-        self.session = None
+        self.__session = None
 
     def get_connection(self):
         if self.engine is None:
+            # "postgresql+psycopg2://postgres:1321@localhost:5432/workout-dev"
             self.engine = create_engine(
-                url=f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+                url=f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
             )
 
         return self.engine
 
-    def get_session(self):
-        if self.session is None:
-            self.session = sessionmaker(bind=self.engine)
+    @property
+    def session(self):
+        if self.__session is None:
+            self.__session = sessionmaker(bind=self.engine)
+        return self.__session
